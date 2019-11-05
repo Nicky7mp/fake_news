26db0cde56d25956bcf18e4ef3abad36c591fdba
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-05 15:33:41 +0000

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default Footer = (props) => {
+    return (
+        <div className="footerMainDiv">
+            <img className="footerLogo" src={props.logo} alt="Fake-news logo" />
+
+            <div className="rights">
+                <h6>All rights reserved. © 2019 FakeNews Publications Ltd.</h6>
+            </div>
+
+            <div className="cookie">
+                <h6>Cookie Settings</h6>
+            </div>
+
+            <div className="terms">
+                <h6>Terms and Conditions</h6>
+            </div>
+
+            <div className="privacy">
+                <h6>Privacy policy</h6>
+            </div>
+            
+        </div>
+    )
+}
