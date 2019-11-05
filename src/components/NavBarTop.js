adc0962cf6491594ed41f6a98a9f181d01e2959b
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-05 14:11:55 +0000

    Topnavbar component

---
+++ src/components/NavBarTop.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+
+// Do we need this? 
+// Do I have to use <a> instead of <Link>
+import {Link} from 'react-router-dom'
+
+export default  NavBarTop = (props) => {
+    return (
+        <nav className="navBarTop">
+            <ul>
+                <img className="navBarLogo" src={props.img} alt="Fake-news logo"/>
+
+                <li>
+                    <Link to="/">News</Link>
+                </li>
+
+                <li>
+                    <Link to="/opinion">Opinion</Link>
+                </li>
+
+                <li>
+                    <Link to="/lifestyle">LifeStyle</Link>
+                </li>
+
+                <li>
+                    <Link to="/culture">Culture</Link>
+                </li>
+
+                <li>
+                    <Link to="/sport">Sport</Link>
+                </li>
+
+                <li>
+                     <Link to="/">Search</Link>
+                </li>
+
+                <li>
+                     <Link to="/">Sign in</Link>
+                </li>
+            </ul>
+        </nav>
+        
+    );
+}
