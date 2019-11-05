792aeaff2fc036403cab1ff989a10d9315b1e02b
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-05 16:23:57 +0000

    export default changes

--- src/components/SmallCards.js
+++ src/components/SmallCards.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default SmallCards = (props) => {
+const SmallCards = (props) => {
     return (
         <div className="bodyDivSmallCards">
 
@@ -17,3 +17,6 @@ export default SmallCards = (props) => {
         </div>
     )
 }
+
+
+export default SmallCards
