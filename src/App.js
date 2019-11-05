c44dddd93a347b03dc71cf8a6f6d84a18274eda3
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-05 12:21:42 +0000

    added some news titles

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 import Jacobgc from './images/jacobgeeclarke.jpeg'
 import Jacob from './images/Jacobmanswins.jpeg'
 import Natalja from './images/natalja.jpg'
-import Natalja2 from './images/natalja2.jpg'
 import Pav from './images/pav.jpg'
 import Pav2 from './images/pav2.jpg'
 import Vicky from './images/vicky.jpg'
+import Maarja from './images/Maarja.jpg'
 
 function App() {
   return (
     <div className="App">
-    <Box title="Dean devasted as he loses in-work boyfriend to Jacob G Clarke"></Box>
-    <Box title="Keen Greenfingered lady causes devastaion to all plants in garden centre"></Box>
-    <Box title="The question everyone is asking, Who's Sarah?"></Box>
+    <Box img= "Dean devasted as he loses in-work boyfriend to Jacob G Clarke"></Box>
+    <Box img={Vicky} title="Keen Greenfingered lady causes devastaion to all plants in garden centre"></Box>
+    <Box img={Natalja} title="The question everyone is asking, Who's Sarah?"></Box>
+    <Box img={Maarja} title="Woman breaks mans arm in thumb war."></Box>
     </div>
   );
 }
