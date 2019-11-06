08c7f146396c84a3f2aef62ee10c2ce419fb2e68
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-06 12:09:59 +0000

    updated news stories

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import Pav2 from './images/pav2.jpg'
 import Vicky from './images/vicky.jpg'
 import Maarja from './images/Maarja.jpg'
 import Train from './images/train.jpg'
-import Arsenal from './images/Arsenal.jpg'
+// import Arsenal from './images/Arsenal.jpg'
 import Telmo from './images/Telmo.png'
 import Blinded from './images/blinded.jpg'
 import MacSmack from './images/macsmack.jpg'
-import Pigs from './images/pigs.jpg'
-import Pineapple from './images/pineapple.jpg'
+// import Pigs from './images/pigs.jpg'
+// import Pineapple from './images/pineapple.jpg'
+import Hamza from './images/Hamza.jpg'
 import Schools from './images/w3.jpeg'
 import Raisins from './images/Raisins.jpeg'
 import Quagmire from './images/giggity.jpg'
@@ -21,6 +22,7 @@ import Nicky from './images/Nicky.JPG'
 import Spicy from './images/spicyfood.png'
 import Jon from './images/jon.jpg'
 import Sharp from './images/sharpAd.jpg'
+import Logo from './images/logo.png'
 import Southcoders1 from './images/southcoders1.jpg'
 import Southcoders2 from './images/southcoders2.jpg'
 import SmallCards from '../src/components/SmallCards'
@@ -38,18 +40,19 @@ class App extends React.Component{
   return (
     
     <div className="App">
-      {/* <NavBarTop /> */}
+      {/* <NavBarTop img={Logo}/> */}
       <MiddleCards img={Jacob} title="Man finally wins his first Ping-Pong match against a 3 year old" subcategory="Not really a sport" />
       <MiddleCards img={Jacobgc} title="Dean devasted as he loses in-work boyfriend to Jacob G Clarke" subcategory="Guylove" />
       <MiddleCards img={Vicky} title="Keen Greenfingered lady sets fire to all plants in garden centre in mad ZenGarden rage" subcategory="Vegetarians" />
       <MainCard img={Natalja} title="The question everyone is asking, Who's Sarah?" />
       <SmallCards img={Maarja} title="Woman breaks mans arm in thumb war." subcategory="The Hulkess" />
       <SmallCards img={Train} title="Man retires after earning 1m in delayed train fares in 3 months" subcategory="Money" />
-      <SmallCards img={Arsenal} title="Arsenal lose AGAIN!" subcategory="Sports" />
+      {/* <SmallCards img={Arsenal} title="Arsenal lose AGAIN!" subcategory="Sports" /> */}
+      <SmallCards img={Hamza} title="Hamza missing classes due to advising Larry Page on running google more efficiently" subcategory="HyperTech" />
       <SmallCards img={Pav} title="Polish man changes his name to 'Pepe le Pav'" subcategory="World" />
       <SmallCards img={Raisins} title="Sneaky raisins finding there way into peoples food" subcategory="Wrong" />
-      <SmallCards img={Pineapple} title="Pineapple does not belong on a pizza no matter what Maarja says"  subcategory="Food" />
-      <SmallCards img={Pigs} title="Unpopular opinion reveals pigs in blankets aint all that.." subcategory="Unpopular Opinons" />
+      {/* <SmallCards img={Pineapple} title="Pineapple does not belong on a pizza no matter what Maarja says"  subcategory="Food" /> */}
+      {/* <SmallCards img={Pigs} title="Unpopular opinion reveals pigs in blankets aint all that.." subcategory="Unpopular Opinons" /> */}
       <SmallCards img={Schools} title="W3 Schools is officially better than MDN, says Jacob" subcategory="Truth" />
       <SmallCards img={Blinded} title="Maarja blinds the editor of fake news by making her look at bright lights" subcategory="Torture" />
       <MiddleCards img={Telmo} title="Portuguese man says 'Winner Winner Chicken Dinner' for the first time" subcategory="World" />
@@ -60,7 +63,7 @@ class App extends React.Component{
       <SmallCards img={Nicky} title="Nicky is now 18 weeks in on the course and still doesnt know anything" subcategory="Numbnuts" />
       <SmallCards img={Quagmire} title="How much Giggity is too much Giggity?" subcategory="Filth" />
       {/* <NavBarBottom /> */}
-      <Footer />
+      <Footer img={Logo}/>
     </div>
   );
   }
